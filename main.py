37239cb535778fa7be84f5a2459be601bd532a49
RyanZem <72454408+RyanZem@users.noreply.github.com>
2021-04-15 15:32:43 +0300

    Add kick command to the bot

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import discord
 
 bot.run(TOKEN)
+
+@commands.has_permissions(kick_members=True)
+@bot.command()
+async def kick(ctx, user: discord.Member, *, reason="No reason provided!"):
+        await user.kick(reason=reason)
+        kick = discord.Embed(title=f":boot: Kicked {user.name}!", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
+        await ctx.message.delete()
+        await ctx.channel.send(embed=kick)
+        await user.send(embed=kick)
