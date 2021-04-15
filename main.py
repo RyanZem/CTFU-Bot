b99315e76d73ab3cfc188d4ee25e88cad8a83e8e
Toby1710 <69259430+DiamondPlane@users.noreply.github.com>
2021-04-15 15:34:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="-")
+
+@bot.event
+async def on_message(ctx):
+    if message.content.lower() == "hi ctfu bot":
+    await ctx.send("Hello! :wave:")
+
 
 bot.run(TOKEN)
