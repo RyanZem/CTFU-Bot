5bfa0fd6c26a3607352662626c94dafff7ecf3fd
RyanZem <72454408+RyanZem@users.noreply.github.com>
2021-04-15 15:44:39 +0300

    Add mute command

--- main.py
+++ main.py
@@ -11,11 +11,22 @@ async def on_message(message):
 
 @bot.command()
 @commands.has_permissions(kick_members=True)
-async def kick(ctx, member: discord.Member, *, reason="No reason provided!"):
+async def kick(ctx, user: discord.Member, *, reason="No reason provided!"):
         await user.kick(reason=reason)
-        kick = discord.Embed(title=f"Kicked {member}!", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
+        kick = discord.Embed(title=f":boot: Kicked {user.name}!", description=f"Reason: {reason}\nBy: {ctx.author.mention}")
+        await ctx.message.delete()
         await ctx.channel.send(embed=kick)
-        dm_kick = discord.Embed(title=f"You were kicked!", description = f"Reason: {reason}\nBy: {ctx.message.author}\nServer: {ctx.guild.name}")
-        await member.send(embed=kick)
+        await user.send(embed=kick)
+
+@bot.command(pass_context = True)
+async def mute(ctx, member: discord.Member):
+     if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '194151340090327041':
+        role = discord.utils.get(member.server.roles, name='Muted')
+        await bot.add_roles(member, role)
+        embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
+        await bot.say(embed=embed)
+     else:
+        embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
+        await bot.say(embed=embed)        
 
 bot.run(TOKEN)
